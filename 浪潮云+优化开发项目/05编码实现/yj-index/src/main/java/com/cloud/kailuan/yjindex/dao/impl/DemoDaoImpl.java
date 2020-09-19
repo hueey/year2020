9f3e30262cd35b19b231ded2f0f800887074b177
admin <1439097053@qq.com>
2020-09-19 10:16:43 +0800

    sdfsdfsfsdf

--- 浪潮云+优化开发项目/05编码实现/yj-index/src/main/java/com/cloud/kailuan/yjindex/dao/impl/DemoDaoImpl.java
+++
@@ -1,8 +0,0 @@
-package com.cloud.kailuan.yjindex.dao.impl;
-
-import com.cloud.kailuan.yjindex.dao.IDemoDao;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class DemoDaoImpl  implements IDemoDao {
-}
