f1b0ea1c783dc945999122666219dc1d594aed95
superMan791 <yuzhiping123>
2020-09-20 14:17:23 +0800

    weeeeeeeeeeeeeeeeeeeee

--- 浪潮云+优化开发项目/05编码实现/yj-index/src/main/java/com/cloud/kailuan/yjindex/dao/ILcFileInfoDao.java
+++ 浪潮云+优化开发项目/05编码实现/yj-index/src/main/java/com/cloud/kailuan/yjindex/dao/ILcFileInfoDao.java
@@ -1,8 +1,8 @@
 package com.cloud.kailuan.yjindex.dao;
 
-import com.cloud.kailuan.yjindex.db.tables.LcFileInfo;
-import com.cloud.kailuan.yjindex.db.tables.records.LcFileInfoRecord;
+
+import com.cloud.kailuan.yjindex.entity.LcFileInfoEntity;
 
 public interface ILcFileInfoDao {
-    void save(LcFileInfo lcFileInfo);
+    void save(LcFileInfoEntity lcFileInfoEntity);
 }
